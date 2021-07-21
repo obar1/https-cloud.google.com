ca7d8b11f2cf8c7493da772b7929a672990536d1
mario amatucci <mamat@softserveinc.com>
2021-07-21 21:30:24 +0200

    wip #6

--- https:§§cloud.google.com§api-gateway§docs§authenticate-service-account/runme.sh
+++ https:§§cloud.google.com§api-gateway§docs§authenticate-service-account/runme.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -u
 set -o pipefail
+set -x
 
 source "${BASE_PATH}"/_core/tools.sh
 
@@ -24,6 +25,9 @@ function create_api() { #
    gcloud api-gateway apis create $API_ID --project=$PROJECT_ID
 }
 
+function enble_api() { #
+   gcloud services enable $API_ID-"${1}".apigateway.$PROJECT_ID.cloud.goog
+}
 
 function deploy_api_config() { #
    gcloud api-gateway api-configs create $API_CONFIG_ID --api=$API_ID --openapi-spec=$API_DEFINITION --project=$PROJECT_ID --backend-auth-service-account="$SA_OAUTH_API_ID@$PROJECT_ID.iam.gserviceaccount.com"
@@ -31,10 +35,6 @@ function deploy_api_config() { #
 
 function describe_api_config() { #
    gcloud api-gateway api-configs describe $API_CONFIG_ID --api=$API_ID --project=$PROJECT_ID
-}
-
-function enble_api() { #
-   gcloud services enable $API_ID-"${1}".apigateway.$PROJECT_ID.cloud.goog
 }
 
 function deploy_api_config_to_gatway() { #
@@ -46,72 +46,60 @@ function describe_gatway() { #
    gcloud api-gateway gateways describe $GATEWAY_ID --location=$GCP_REGION --project=$PROJECT_ID
 }
 
-
-
 ## Cleaning up
 
 function cleaning_up() { #
    # Deleting a gateway
-   gcloud api-gateway gateways delete $GATEWAY_ID --location=$GCP_REGION --project=$PROJECT_ID --async --quiet || true
+   gcloud api-gateway gateways delete $GATEWAY_ID --location=$GCP_REGION --project=$PROJECT_ID --quiet || true
 
    # delete api config
-   gcloud api-gateway api-configs delete $API_CONFIG_ID --api=$API_ID --project=$PROJECT_ID --async --quiet || true
+   gcloud api-gateway api-configs delete $API_CONFIG_ID --api=$API_ID --project=$PROJECT_ID --quiet || true
 
    # delete api
-   gcloud api-gateway apis delete $API_ID --project=$PROJECT_ID --async --quiet || true
+   gcloud api-gateway apis delete $API_ID --project=$PROJECT_ID --quiet || true
 }
 
 ## Making an authenticated request to an API Gateway API
 
 function client_invocation { #
-   defaultHostname="http://${1}"
    python call.py $defaultHostname/hello $defaultHostname SA_PRIVATE_KEY.json "$SA_OAUTH_API_ID@$PROJECT_ID.iam.gserviceaccount.com"
 }
 
 function echo_curl_invoation() { #
-   defaultHostname="http://${1}"
-   TOKEN=123-abc
-   echo curl --request GET --header "Authorization: Bearer ${TOKEN}" $defaultHostname/hello
+   echo curl --request GET --header '"Authorization: Bearer "{$TOKEN}' $defaultHostname/hello
 }
-
-set -x
 
 # params
 
-SA_OAUTH_API_ID=obar1-oauth-000-sa-id
+SA_OAUTH_API_ID=sa-oauth-api-id
 API_DEFINITION='api-config-definition.yaml'
 
-ID=obar1-oauth-001 
+ID=obar1-oauth-007
 
 API_ID=$ID-api-id
 API_CONFIG_ID=$ID-api-config-id
 GATEWAY_ID=$ID-gateway-id
 case "${1}" in
-1 | 'r') ## run
-
+1 | 'r') ## runx
    # create_sa
    # gcloud iam service-accounts list
    # add_role2sa
    # save_key_sa
 
    # create_api
-   # deploy_api_config
+   enble_api '1tcz3faqmtksc'
+   deploy_api_config
    # describe_api_config
-   # enble_api '0t5pd2sbheb8b'
-   # deploy_api_config_to_gatway
+   deploy_api_config_to_gatway
    # describe_gatway
-
    ;;
 2 | 'i') ## invocation
-   client_invocation 'obar1-oauth-001-gateway-id-d6vhs2p6.uc.gateway.dev'
-   echo_curl_invoation 'obar1-oauth-001-gateway-id-d6vhs2p6.uc.gateway.dev'
+   defaultHostname='https://obar1-oauth-007-gateway-id-d6vhs2p6.uc.gateway.dev'
+   client_invocation 
+   echo_curl_invoation
    ;;
 3 | 'c') ## clean up
-
    cleaning_up
-
    ;;
 *) info "$0" ;;
 esac
-
-set +x
